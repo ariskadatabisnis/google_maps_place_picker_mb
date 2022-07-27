de2eee48eb75e7f348ace5b3994ace595da3823e
ariska <ariska@databisnis.id>
2022-07-27 16:51:28 +0700

    initial

--- lib/src/autocomplete_search.dart
+++ lib/src/autocomplete_search.dart
@@ -18,7 +18,7 @@ class AutoCompleteSearch extends StatefulWidget {
       this.hintText,
       this.searchingText = "Searching...",
       this.hidden = false,
-      this.height = 40,
+      this.height = 46,
       this.contentPadding = EdgeInsets.zero,
       this.debounceMilliseconds,
       this.onSearchFailed,
@@ -33,7 +33,8 @@ class AutoCompleteSearch extends StatefulWidget {
       this.initialSearchString,
       this.searchForInitialValue,
       this.autocompleteOnTrailingWhitespace})
-      : super(key: key);
+      : assert(searchBarController != null),
+        super(key: key);
 
   final String? sessionToken;
   final String? hintText;
@@ -104,8 +105,8 @@ class AutoCompleteSearchState extends State<AutoCompleteSearch> {
         height: widget.height,
         padding: const EdgeInsets.only(right: 10),
         color: Theme.of(context).brightness == Brightness.dark ? Colors.black54 : Colors.white,
-        borderRadius: BorderRadius.circular(20),
-        elevation: 8.0,
+        borderRadius: BorderRadius.circular(8),
+        elevation: 0,
         child: Row(
           children: <Widget>[
             SizedBox(width: 10),
@@ -314,7 +315,7 @@ class AutoCompleteSearchState extends State<AutoCompleteSearch> {
   clearOverlay() {
     _clearOverlay();
   }
-  
+
   /// This allows a value of type T or T? to be treated as a value of type T?.
   ///
   /// We use this so that APIs that have become non-nullable can still be used
